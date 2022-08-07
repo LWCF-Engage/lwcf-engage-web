97b338be1056a5e428b409a913b2792e499b407c
devzana <lozanascbusiness@gmail.com>
2022-08-08 00:32:18 +0800

    Initial commit for this platform's repository

---
+++ resources/js/app.jsx
@@ -0,0 +1,7 @@
+import './bootstrap';
+import '../css/app.css'
+
+import ReactDOM from 'react-dom/client';        
+import Home from './Page/Home';
+
+ReactDOM.createRoot(document.getElementById('app')).render(<Home />);
