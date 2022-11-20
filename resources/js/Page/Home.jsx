d7deab46f1f1db5dc41c6345af08e1e02d24b618
devzana <lozanascbusiness@gmail.com>
2022-11-20 20:41:13 +0800

    Reinitialized lwcf web to react-vite for light landing page

--- resources/js/Page/Home.jsx
+++
@@ -1,4 +0,0 @@
-export default function Home() {
-  const heading = "Laravel 9 Vite  with React JS";
-  return <div> {heading}</div>;
-}
--- resources/js/app.jsx
+++
@@ -1,7 +0,0 @@
-import './bootstrap';
-import '../css/app.css'
-
-import ReactDOM from 'react-dom/client';        
-import Home from './Page/Home';
-
-ReactDOM.createRoot(document.getElementById('app')).render(<Home />);
